d436c2354520f60aa3a7d86bfa0766083c71f9a4
Bowman Edebohls <bowman@edebohls.com>
2024-12-22 14:20:57 -0800

    fixed offset in paragraph boxes

---
+++ bowAI.js
@@ -0,0 +1,7 @@
+document.getElementById("aiInput").addEventListener("keypress", e => {
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+
+        e.currentTarget.closest("form").submit();
+    }
+});
